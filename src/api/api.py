4165006ec607ffeb9e411802e1957d95853854d0
Alexander Järnström <alexander.jarnstrom@gmail.com>
2024-09-24 13:47:06 +0200

    Making TOX happy

--- src/api/api.py
+++
@@ -1,4 +0,0 @@
-from ollama_connector import test
-
-if __name__ == "__main__":
-    test()
--- src/api/ollama_connector.py
+++ src/api/ollama_connector.py
@@ -1,5 +1,4 @@
-'''
-Handle ollama connection requests.
+"""Handle ollama connection requests.
 
 Classes:
     OllamaConfig
@@ -7,7 +6,8 @@ Classes:
 Functions:
     parse_config(config_path) -> OllamaConfig
     test()
-'''
+
+"""
 
 import tomllib
 import logging
@@ -15,13 +15,12 @@ from datetime import date
 
 from ollama import Client
 
-logging.basicConfig(filename=f'ollama-connector-{date.today()}.log', level=logging.INFO)
+logging.basicConfig(filename=f"ollama-connector-{date.today()}.log", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 class OllamaConfig:
-    '''
-    Class to represent a Ollama config.
+    """Class to represent a Ollama config.
 
     Attributes:
         address: str
@@ -32,59 +31,56 @@ class OllamaConfig:
     Methods:
         get_url(): str
             Returns 'http://<address>:<port>'
-    '''
 
+    """
 
     def __init__(self, config: dict) -> None:
+        """Set the config attributes."""
         self.address = config["Host"]["Address"]
         self.port = config["Host"]["Port"]
-
+        self.model = config["LLM"]["Model"]
 
     def get_url(self) -> str:
-        '''
-        Returns full ollama  url.
+        """Return full ollama  url.
 
-            Returns:
-                url : str
-                    full url
-        '''
-        
+        Returns:
+            url : str
+                full url
+
+        """
         return f"http://{self.address}:{self.port}"
 
 
 def parse_config(config_path: str) -> OllamaConfig:
-    '''
-    Reads config.toml and returns the parsed content as a OllamaConfig 
-    object.
+    """Load ollama config and return it.
 
-        Returns:
-            ollama_config : OllamaConfig
-                Ollama config object
-    '''
+    Returns:
+        ollama_config : OllamaConfig
+            Ollama config object
 
+    """
     ollama_config: OllamaConfig
 
-    logger.info(f'Opening {config_path}.')
+    logger.info(f"Opening {config_path}.")
     with open(config_path, "rb") as f:
-        logger.debug(f'Reading {config_path}.')
+        logger.debug(f"Reading {config_path}.")
         data = tomllib.load(f)
         ollama_config = OllamaConfig(data)
-    
-    logger.info(f'Closed {config_path}.')
+
+    logger.info(f"Closed {config_path}.")
 
     return ollama_config
 
 
 def test():
-    '''
-    Loads current config and sens test request to ollama.
-    '''
-
-    logging.info('Testing connection with a request.')
-    config: OllamaConfig = parse_config('config.toml')
+    """Test connectivity to the llm."""
+    logging.info("Testing connection with a request.")
+    config: OllamaConfig = parse_config("config.toml")
     client: Client = Client(config.get_url())
-    response = client.chat(model="llama3.1", messages=[{"role": "user", "content": "what is a rock?"}])
+    response = client.chat(model=config.model, messages=[{"role": "user", "content": "what is a rock?"}])
 
     print(response)
-    
 
+
+if __name__ == "__main__":
+    test()
