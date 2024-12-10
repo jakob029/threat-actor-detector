9aa3ded1a4630215b1cda1798e7d45b22bd65a59
Alexander Järnström <alexander.jarnstrom@gmail.com>
2024-12-10 19:47:18 +0100

    Banger

--- scripts/install_api.bash
+++ scripts/install_api.bash
@@ -4,18 +4,15 @@
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   echo "Creating .env ..."
   python3 -m venv ../.env
-  source ../.env/bin/activate
 
   echo "Installing requirements ..."
-  pip install -r ../requirements.txt
+  ../.env/bin/pip install -r ../requirements.txt
 
-  echo "Leaving venv ..." 
-  deactivate
-
-  echo "Creating src/api/.env ..."
-  touch ../src/api/.env 
-  echo "
-  # LLM options
+  if [ ! -f ../src/api/.env ]; then 
+    echo "Creating src/api/.env ..."
+    touch ../src/api/.env 
+    echo "
+# LLM options
 TAD_LLM_MODEL=llama3.1                    # Model to use
 TAD_LLM_ADDRESS=http://100.77.88.10       # Ollama address
 TAD_LLM_PREPROPT_PATH=./src/api/prepromt  # Preprompt path
@@ -31,15 +28,21 @@ API_ADDRESS=10.40.0.40
 # Vector database options
 VECTOR_DB_HOST=100.77.88.70     # Vector database host address
 VECTOR_DB_PORT=5000             # Vector database host open port
-  " >> ../src/api/.env
+    " >> ../src/api/.env
 
-  if command -v vim 2>&1 >/dev/null; then
-    echo "Opening vim ..."
-    vim ../src/api/.env
+    if command -v vim 2>&1 >/dev/null; then
+      echo "Opening vim ..."
+      vim ../src/api/.env
+    else
+      echo "Lmao, nano pleb."
+      nano ../src/api/.env
+    fi
   else
-    echo "Lmao, nano pleb."
-    nano ../src/api/.env
+    echo "src/api/.env already exists."
   fi
+
+  echo "Build package ..."
+  ../.env/bin/pip install -e ../
 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   echo "Detected windows. -_-"
