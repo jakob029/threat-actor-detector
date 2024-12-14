d9b6831462e0583253f108b513e2c267a428fe1f
jakob029 <jacke029@gmail.com>
2024-12-14 21:25:17 +0100

    Fix of vector db buggs

--- src/vector_db_api/endpoints/group_analyzer.py
+++ src/vector_db_api/endpoints/group_analyzer.py
@@ -2,13 +2,15 @@
 
 from flask_restful import Resource
 from flask_restful.reqparse import RequestParser
+from src.vector_db_api.build_dataset.build_chroma_db import VectorDB
+
 from src.vector_db_api.build_dataset.run import build_vector_database
 
 
 class GroupAnalyzer(Resource):
     """Group analyzer endpoint for vector db API."""
 
-    vector_databases: tuple = tuple()
+    vector_databases: VectorDB = build_vector_database()
 
     def get(self) -> tuple:
         """Retrieve a response from the vector database.
@@ -22,16 +24,11 @@ class GroupAnalyzer(Resource):
             Response given from the vector database including 'ids' for
             represented by names of APT groups & distances to those ids.
         """
-        if not self.vector_databases:
-            self.vector_databases = build_vector_database()
-
         parser: RequestParser = RequestParser()
         parser.add_argument("prompt", type=str, required=True)
         args = parser.parse_args(strict=True)
 
-        (description_builder,) = self.vector_databases
-
-        relation_response = description_builder.collection.query(
+        relation_response = self.vector_databases.collection.query(
             query_texts=args["prompt"], n_results=10, include=["distances"]
         )
 
