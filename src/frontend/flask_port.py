08fa1ad1448b3d65d008de55b1465b51387c422d
FilipDar <filip.darinder14@gmail.com>
2024-12-14 16:11:27 +0100

    tox pass plz

--- src/frontend/flask_port.py
+++ src/frontend/flask_port.py
@@ -24,11 +24,14 @@ def homepage():
         uid=uid,  # Pass uid to the template
     )
 
-@app.route('/info.html')
+
+@app.route("/info.html")
 def info_page():
+    """Route to info page."""
     print(f"Session on info page: {session}")  # Debug log
-    is_logged_in = 'uid' in session  # Check if the user is logged in
-    return render_template('info.html', is_logged_in=is_logged_in, username=session.get('username'))
+    is_logged_in = "uid" in session  # Check if the user is logged in
+    return render_template("info.html", is_logged_in=is_logged_in, username=session.get("username"))
+
 
 @app.route("/accept-cookies", methods=["POST"])
 def accept_cookies():
